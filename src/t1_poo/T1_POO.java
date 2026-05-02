985fdc1cb0b79ac754412f8def78c387a3309673
Cristian <N00499227@upn.pe>
2026-05-02 13:23:44 -0500

    Codigo para solicitar datos al usuario

--- src/t1_poo/T1_POO.java
+++ src/t1_poo/T1_POO.java
@@ -17,7 +17,43 @@ public class T1_POO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Alumno alu = new Alumno();
+        AlumnoControlador ac = new AlumnoControlador();
         
+        
+        System.out.println("-----------Registro de Alumnos-----------");
+        
+            System.out.println("Ingrese Tipo de Documento (DNI / CARNET)"); String td = sc.nextLine();
+        alu.setTipoDocumento(td);
+         
+        
+            System.out.println("Ingrese Numero de Documento"); String nd = sc.nextLine();
+        alu.setNumeroDocumento(nd);
+         
+
+        System.out.println("Apellido Paterno:"); String ap=sc.nextLine();
+        alu.setApellidoPaterno(ap);
+          
+        System.out.println("Apellido Materno"); String am = sc.nextLine();
+        alu.setApellidoMaterno(am);
+          
+        
+        
+        System.out.println("Nombre"); String na = sc.nextLine();
+        alu.setNombre(na);
+        
+        System.out.println("Ingrese la edad del Estudiante"); int ed = Integer.parseInt(sc.nextLine());
+                    alu.setEdad(ed);
+                
+        String nsocio = alu.asignarNivelSocioeconomico(sc);
+        alu.setNivelSocioeconomico(nsocio);
+        
+        String bec = alu.asignarBeca(sc);
+        alu.setBeca(bec);
+        
+        ac.AgregarALumno(alu);
+        ac.MostrarAlumno();
     }
     
 }
