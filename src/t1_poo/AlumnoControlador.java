7352f6d31f1f55247ee25fe8b7250232339e87ec
Cristian <N00499227@upn.pe>
2026-05-02 13:20:53 -0500

    Clase Controlador

--- src/t1_poo/AlumnoControlador.java
+++ src/t1_poo/AlumnoControlador.java
@@ -12,6 +12,24 @@ import java.util.ArrayList;
  * @author zakkc
  */
 public class AlumnoControlador {
+    public ArrayList<Alumno> listaAlumno = new ArrayList<>();
     
+    public AlumnoControlador()
+    {
+        this.listaAlumno = new ArrayList<>();
+    }
+    
+    public void AgregarALumno(Alumno e)
+    {
+        listaAlumno.add(e);
+    }
+    
+    public void MostrarAlumno()
+    {
+        for(Alumno al:listaAlumno)
+        {
+            al.Mostrar();
+        }
+    }
     
 }
